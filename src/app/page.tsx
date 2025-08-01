7b9cc075aeae40e2b512128901fbcf8bda7caf03
Riley Robertson <riley@brighthire.ai>
2025-07-31 21:32:21 -0400

    some strange CSS tinkering to fix mobile flashes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div className="grid grid-rows-1 items-start md:items-center justify-items-center min-h-full p-2 pb-20 gap-16 sm:p-10 md:p-20">
       <ScrollGradient />
+      <Confetti n={20} />
       <main className="flex flex-col gap-[32px] items-center max-w-[850px] mt-2 sm:mt-4 md:mt-0 z-0">
-        <Confetti n={20} />
         <Image
           className="rounded-lg shadow-2xl/100"
           src="/loading-page.jpg"
@@ -44,9 +44,7 @@ export default function Home() {
       </main>
       {/* Wedding Weekend Section */}
       <section className="flex flex-col gap-6 items-center text-center max-w-[850px] mx-2 sm:mx-0">
-        <h3 className="text-2xl font-swash font-bold">
-          The Wedding Weekend
-        </h3>
+        <h3 className="text-2xl font-swash font-bold">The Wedding Weekend</h3>
 
         <p className="text-lg max-w-prose bg-white/80 p-4 rounded-lg shadow">
           <strong className="text-xl font-swash font-bold">
@@ -58,9 +56,7 @@ export default function Home() {
           Near the venue (exact location TBD)
           <br></br>
           <br></br>
-          <strong className="text-xl font-swash font-bold">
-            Ceremony
-          </strong>
+          <strong className="text-xl font-swash font-bold">Ceremony</strong>
           <br />
           May 30, 2026 <br></br>
           Afternoon (exact time TBD) <br></br>
