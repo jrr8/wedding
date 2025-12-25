be88776ea738e3c18bb04fc5332dbf7b709f0ca3
Riley Robertson <riley@brighthire.ai>
2025-12-24 19:13:07 -0800

    Save user in cookie

--- src/app/api/rsvp/route.ts
+++ src/app/api/rsvp/route.ts
@@ -1,20 +1,43 @@
 import { google, sheets_v4 } from "googleapis";
 import { NextResponse, NextRequest } from "next/server";
 import { distance } from "fastest-levenshtein";
+import { cookies } from "next/headers";
 
 const spreadsheetId = process.env.GOOGLE_SHEETS_SHEET_ID;
 const spreadsheetName = process.env.GOOGLE_SHEETS_SHEET_NAME || "RSVP";
+const errorSheetName = "RSVP-errors";
 
 type Sheets = sheets_v4.Sheets;
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
   const name = req.nextUrl.searchParams.get("name")?.trim().toLowerCase() ?? "";
+  const cookieStore = await cookies();
+  const currentUserId = cookieStore.get("user")?.value
 
   try {
     const sheets = getSheets();
-    const names = await getColumn(sheets, "C2:C");
+    const sheet = await getSheet(sheets);
 
+    let currentUserRow = currentUserId
+      ? sheet.find((row) => row.id === currentUserId)
+      : null;
+
+    if (currentUserRow) {
+      return NextResponse.json({
+        closestName: currentUserRow.name,
+      });
+    }
+
+    const names = sheet.map((row) => row.name);
     const closestName = getClosestName(name, names);
+
+    if (closestName) {
+      currentUserRow = sheet.find((row) => row.name === closestName);
+    }
+
+    if (currentUserRow) {
+      cookieStore.set("user", currentUserRow.id);
+    }
 
     return NextResponse.json({ closestName });
   } catch (err) {
@@ -32,7 +55,7 @@ export async function POST(req: NextRequest): Promise<NextResponse> {
     // Append new row
     await sheets.spreadsheets.values.append({
       spreadsheetId,
-      range: getRange("A:D"),
+      range: sheetRange("A:D"),
       valueInputOption: "USER_ENTERED",
       requestBody: {
         values: [
@@ -58,8 +81,17 @@ const getSheets = (): Sheets => {
   return google.sheets({ version: "v4", auth });
 };
 
-const getRange = (range: string): string => {
-  return `${spreadsheetName}!${range}`;
+const sheetRange = (range: string = ""): string => {
+  return range ? `${spreadsheetName}!${range}` : spreadsheetName;
+};
+
+const logError = async (sheets: Sheets, error: string): Promise<void> => {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range: sheetRange(errorSheetName),
+    valueInputOption: "USER_ENTERED",
+    requestBody: { values: [[Date.now(), error]] },
+  });
 };
 
 const getColumn = async <T = string>(
@@ -70,10 +102,29 @@ const getColumn = async <T = string>(
     data: { values },
   } = await sheets.spreadsheets.values.get({
     spreadsheetId,
-    range: getRange(range),
+    range: sheetRange(range),
     majorDimension: "COLUMNS",
   });
   return values?.[0] || [];
+};
+
+const getSheet = async (sheets: Sheets): Promise<SheetRow[]> => {
+  const {
+    data: { values },
+  } = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range: sheetRange(),
+  });
+  if (!values?.length) return [];
+
+  return values.slice(1).map((row) => ({
+    id: row[0],
+    partyId: row[1] || null,
+    name: row[2],
+    rsvpCeremony: row[3] ? row[3] === "TRUE" : null,
+    rsvpWelcome: row[4] ? row[4] === "TRUE" : null,
+    dietaryRestrictions: row[5] || null,
+  }));
 };
 
 const similarity = (a: string, b: string): number => {
@@ -97,4 +148,13 @@ const getClosestName = (name: string, names: string[]): string | null => {
   }
 
   return closestName;
+};
+
+type SheetRow = {
+  id: string;
+  partyId: string | null;
+  name: string;
+  rsvpCeremony: boolean | null;
+  rsvpWelcome: boolean | null;
+  dietaryRestrictions: string | null;
 };
